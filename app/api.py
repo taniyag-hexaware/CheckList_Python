07cae5869bd78be8bde480bb5cb934d2be997b1f
taniyag-hexaware <TaniyaG@hexaware.com>
2021-10-19 19:58:28 +0530

    Logging added

--- app/api.py
+++ app/api.py
@@ -21,6 +21,7 @@ def add_workOrder():
         resp = jsonify("User added successfully")
         # resp ={"_id":id, 'name':_name, 'description':_description} ,
         resp.status_code = 200
+        app.logger.info('WorkOrder sucessfully created!')
         return resp
     
     else:
@@ -30,12 +31,14 @@ def add_workOrder():
 def workOrders():
     workOrders = mongo.db.workorders.find({},{'name':1})
     resp = dumps(workOrders)
+    app.logger.info('WorkOrder sucessfully displayed!')
     return resp
 
 @app.route('/workOrder/<id>')
 def workOrder(id):
     workOrder = mongo.db.workorders.find_one({'_id':ObjectId(id)})
     resp = dumps(workOrder)
+    app.logger.info('WorkOrder sucessfully displayed with id '+id)
     return resp
 
 @app.route('/workOrder/delete/<id>', methods=['DELETE'])
@@ -43,6 +46,7 @@ def workOrderDelete(id):
     mongo.db.workorders.delete_one({'_id':ObjectId(id)})
     resp = jsonify("The workOrder with Id " + id + " has been deleted")
     resp.status_code = 200
+    app.logger.info('WorkOrder sucessfully deleted wit id '+id)
     return resp
 
 @app.route('/workOrder/update/<id>', methods=['PUT'])
@@ -54,6 +58,7 @@ def workOrderUpdate(id):
     if _name and _id and request.method == 'PUT':
         mongo.db.workorders.update_one({'_id':ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},{'$set':{'name':_name, 'description':_description}})
         resp = jsonify("User updated successfully")
+        app.logger.info('WorkOrder sucessfully updated with id '+id)
         resp.status_code = 200
         return resp
     else:
@@ -78,6 +83,7 @@ def add_task():
         resp = jsonify("Task added successfully")
         # resp ={"_id":id, 'task_name':_task_name, 'task_description':_task_description,'wordOrderId':ObjectId(_wordOrderId)} ,
         resp.status_code = 200
+        app.logger.info('Task sucessfully created!')
         return resp
     
     else:
@@ -87,12 +93,14 @@ def add_task():
 def tasks():
     tasks = mongo.db.tasks.find({},{'task_name':1})
     resp = dumps(tasks)
+    app.logger.info('Task sucessfully displayed!')
     return resp
 
 @app.route('/task/<id>')
 def task(id):
     task = mongo.db.tasks.find_one({'_id':ObjectId(id)})
     resp = dumps(task)
+    app.logger.info('Task sucessfully displayed with id '+id) 
     return resp
 
 @app.route('/task/delete/<id>', methods=['DELETE'])
@@ -100,6 +108,7 @@ def taskDelete(id):
     mongo.db.tasks.delete_one({'_id':ObjectId(id)})
     resp = jsonify("The task with Id " + id + " has been deleted")
     resp.status_code = 200
+    app.logger.info('task sucessfully deleted with id '+id) 
     return resp
 
 @app.route('/task/update/<tid>', methods=['PUT'])
@@ -113,6 +122,7 @@ def taskUpdate(tid):
         mongo.db.tasks.update_one({'_id':ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},{'$set':{'name':_name, 'description':_description,'wordOrderId':ObjectId(_wordOrderId)}})
         resp = jsonify("task updated successfully")
         resp.status_code = 200
+        app.logger.info('task successfully updated with id '+tid) 
         return resp
     else:
         return not_found()
@@ -129,7 +139,7 @@ def not_found(error=None):
     }
     resp = jsonify(message)
     resp.status_code = 404
-
+    app.logger.error('404 page not found '+ request.url)
     return resp
 
 
